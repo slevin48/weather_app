79eebf1fc388440f11563ef8937b0133d0dcbab4
slevin48 <yann.debray@gadz.org>
2022-12-10 20:34:37 +0000

    Generate app with ChatGPT

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Weather app [![Open in Streamlit](https://static.streamlit.io/badges/streamlit_badge_black_white.svg)](https://share.streamlit.io/slevin48/weather_app)
+
+Asked ChatGPT:
+
+`Write me the code for a streamlit application calling weather prediction data from the openweather Api with the urllib module and plotting it with plotly`
+
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,34 @@
 import streamlit as st
-import mymodel as m
+from urllib.request import urlopen
+import json
+import plotly.express as px
 
-st.sidebar.write("""# My Weather App
-Forecasting Weather with regression model""")
+def get_weather_data():
+    # Make a request to the OpenWeather API to get the data
+    api_key = st.secrets['key']
+    url = 'http://api.openweathermap.org/data/2.5/forecast?q=Boston,us&units=imperial&appid=' + api_key
+    response = urlopen(url)
+    data = json.loads(response.read())
+    return data
 
-k = st.slider("range",0,40,30)
-st.write(m.run(k))
+def main():
+    st.title('Weather Prediction App')
+
+    # Get the data from the OpenWeather API
+    data = get_weather_data()
+
+    # Extract the relevant data from the API response
+    dates = [x['dt_txt'] for x in data['list']]
+    temps = [x['main']['temp'] for x in data['list']]
+    humidity = [x['main']['humidity'] for x in data['list']]
+
+    # Create a plotly line chart to visualize the data
+    fig1 = px.line(x=dates, y=temps, title='Temperature Forecast')
+    st.plotly_chart(fig1)
+
+    # Create a plotly scatter plot to visualize the humidity data
+    fig2 = px.scatter(x=dates, y=humidity, title='Humidity Forecast')
+    st.plotly_chart(fig2)
+
+if __name__ == '__main__':
+    main()
